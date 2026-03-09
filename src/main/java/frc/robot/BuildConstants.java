c6f89136789b418edcfaf34e43e46890f8e51c3c
Evan <c26phillips.e@delanoschools.org>
2026-03-09 14:04:11 -0500

    Updated JSON Library to bugfix

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -12,8 +12,8 @@ public final class BuildConstants {
   public static final String GIT_SHA = "7ae2f45eda4bbcd8159ba1a34ec4ec22be14050f";
   public static final String GIT_DATE = "2026-03-08 21:00:07 EDT";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2026-03-08 22:38:25 EDT";
-  public static final long BUILD_UNIX_TIME = 1773023905392L;
+  public static final String BUILD_DATE = "2026-03-09 15:02:53 EDT";
+  public static final long BUILD_UNIX_TIME = 1773082973766L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
