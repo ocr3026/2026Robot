e4f478c583e8487d1d07487286bb3d6a8225c468
Evan <c26phillips.e@delanoschools.org>
2026-03-10 13:35:31 -0500

    small fix with .jar file for Library

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -8,12 +8,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2026Robot";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 37;
-  public static final String GIT_SHA = "1190591e289a9f22670ba31700a7e4825423fece";
-  public static final String GIT_DATE = "2026-03-09 23:40:41 EDT";
+  public static final int GIT_REVISION = 38;
+  public static final String GIT_SHA = "ad67075634ff2ae4ad1f85e4f462d444b01d029a";
+  public static final String GIT_DATE = "2026-03-10 14:22:44 EDT";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2026-03-10 14:20:58 EDT";
-  public static final long BUILD_UNIX_TIME = 1773166858275L;
+  public static final String BUILD_DATE = "2026-03-10 14:34:28 EDT";
+  public static final long BUILD_UNIX_TIME = 1773167668921L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
