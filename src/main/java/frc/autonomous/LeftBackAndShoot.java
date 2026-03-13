4086fb83899e01e8b667374f4e40ebff98bd43ea
Evan <c26phillips.e@delanoschools.org>
2026-03-13 17:29:50 -0500

    Added them now

---
+++ src/main/java/frc/autonomous/LeftBackAndShoot.java
@@ -0,0 +1,23 @@
+package frc.autonomous;
+
+import frc.robot.subsystems.drive.DriveSubsystem;
+import frc.robot.subsystems.hopper.HopperSubsystem;
+import frc.robot.subsystems.intake.IntakeSubsystem;
+import frc.robot.subsystems.shooter.ShooterSubsystem;
+
+public class LeftBackAndShoot extends AutoBase{
+    public LeftBackAndShoot(
+      HopperSubsystem hopper,
+      ShooterSubsystem shooter,
+      IntakeSubsystem intake,
+      DriveSubsystem drive) {
+    super(hopper, shooter, intake, drive);
+    addCommands(delayStartTime());
+    // addCommands(setStartPose(Paths.driveBackSimple));
+    addCommands(lowerIntake(intake));
+    addCommands(pathFindToPoseLocked(drive, Paths.aimTurret, Paths.leftShoot));
+    // addCommands(shootFuel(hopper, shooter));
+    // addCommands(HopperCommands.runHopper(hopper, -200));
+    addCommands(runHopperAndShooter(hopper, shooter));
+  }
+}
---
+++ src/main/java/frc/autonomous/RightBackAndShoot.java
@@ -0,0 +1,27 @@
+/* Generated and Formatted by yours truly <3*/
+package frc.autonomous;
+
+import frc.robot.subsystems.drive.DriveSubsystem;
+import frc.robot.subsystems.hopper.HopperSubsystem;
+import frc.robot.subsystems.intake.IntakeSubsystem;
+import frc.robot.subsystems.shooter.ShooterSubsystem;
+
+public class RightBackAndShoot extends AutoBase {
+
+  public RightBackAndShoot(
+      HopperSubsystem hopper,
+      ShooterSubsystem shooter,
+      IntakeSubsystem intake,
+      DriveSubsystem drive) {
+    super(hopper, shooter, intake, drive);
+    addCommands(delayStartTime());
+    // addCommands(setStartPose(Paths.driveBackSimple));
+    addCommands(lowerIntake(intake));
+    addCommands(pathFindToPoseLocked(drive, Paths.aimTurret, Paths.rightShoot));
+    // addCommands(followPath(Paths.driveBackSimple));
+
+    // addCommands(shootFuel(hopper, shooter));
+    // addCommands(HopperCommands.runHopper(hopper, -200));
+    addCommands(runHopperAndShooter(hopper, shooter));
+  }
+}
